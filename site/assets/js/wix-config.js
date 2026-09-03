5c4972088a4f108bac01ddbd803f615454d39a6f
reznetllc-bit <bradonshymes@gmail.com>
2026-09-03 11:02:25 -0400

    Bind structured RezNet intake fields

--- site/assets/js/wix-config.js
+++ site/assets/js/wix-config.js
@@ -12,6 +12,13 @@ window.REZNET_WIX = Object.freeze({
     lastName: "last_name_b88c",
     email: "email_673d",
     phone: "phone_6a4b",
-    message: "message_7634"
+    message: "message_7634",
+    cityZip: "city_zip_372e",
+    role: "role_a6e4",
+    propertyStage: "property_stage_6098",
+    primaryConcern: "primary_concern_10c4",
+    squareFootage: "square_footage_f1c8",
+    timeline: "timeline_4a55",
+    contactPermission: "contact_permission_53dc"
   })
 });
